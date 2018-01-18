92dc9fc9cbd97a3e7fa45d69e4688062923ece04
maksym <maksym@mmm.mmm>
2018-01-18 14:28:17 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port=process.env.PORT || 3000;
 let app = express();
 
 
@@ -43,7 +44,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        welcomeMessage: 'Hello, my name is Maksym!',
+        welcomeMessage: 'Hello, my name is Maksym!!!!',
         pageTitle: 'About Page',
     });
 });
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
